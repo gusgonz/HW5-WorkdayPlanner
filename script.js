4bd06b8370fa3707a1ce935774dd4e9b2e19117e
GusGo <gus.gonzalez.15@gmail.com>
2020-01-10 21:52:38 -0500

    have added a lot to render todos function. it works. also added so that if you save a space or empty todo, it will clear that item from local storage so it will not render next time they come back.

--- script.js
+++ script.js
@@ -3,17 +3,31 @@ $('.saveBtn').click(function() {
 	// console.log(typeof time);
 
 	var todoHourClass = '.todo.' + time;
-	console.log(todoHourClass);
+	// console.log(todoHourClass);
 
 	var todoItem = $(todoHourClass).val().trim();
-	console.log(todoItem.length);
+	// console.log(todoItem.length);
 
 	if (todoItem !== '') {
 		localStorage.setItem(time, todoItem);
 		$(todoHourClass).addClass('saved-bold');
+	} else {
+		localStorage.removeItem(time);
 	}
 });
 
 const hourArray = [ 'nine', 'ten', 'eleven', 'noon', 'one', 'two', 'three', 'four', 'five' ];
 
-function renderTodos() {}
+function renderTodos() {
+	for (x of hourArray) {
+		var todoItem = localStorage.getItem(x);
+		console.log(todoItem);
+
+		if (todoItem !== null) {
+			var todoHourClass = '.todo.' + x;
+			$(todoHourClass).text(todoItem);
+		}
+	}
+}
+
+renderTodos();
