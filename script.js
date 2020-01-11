fa4d090b08a907768b32d73e367d395d5cda5486
GusGo <gus.gonzalez.15@gmail.com>
2020-01-11 16:33:46 -0500

    make the render color function in one go and it works! using flags, if statements and nested for loops.

--- script.js
+++ script.js
@@ -26,16 +26,65 @@ function renderTodos() {
 		}
 	}
 }
+var hourNumsArray = [ '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm' ];
 
 function renderTime() {
 	var currentDay = moment().format('dddd, MMMM Do');
 	$('#currentDay').text(currentDay);
+}
 
+function renderColors() {
 	var currentHour = moment().format('h');
 	var ampm = moment().format('a');
-	// console.log(currentHour);
-	// console.log(ampm);
+
+	// FOR TESTING
+	// var currentHour = '3';
+	// var ampm = 'pm';
+
+	var both = currentHour + ampm;
+	var isequalFlag = false;
+	var flag = false;
+
+	// this one is a doozy
+	for (var i = 0; i < hourNumsArray.length; i++) {
+		if (both === hourNumsArray[i]) {
+			isequalFlag = true;
+
+			var todoHourClass = '.todo.' + hourArray[i];
+			$(todoHourClass).addClass('present');
+
+			for (var n = 0; n < hourArray.length; n++) {
+				if (flag) {
+					var futureClass = '.todo.' + hourArray[n];
+					$(futureClass).addClass('future');
+				} else if (both === hourNumsArray[n]) {
+					flag = true;
+				} else {
+					var pastClass = '.todo.' + hourArray[n];
+					$(pastClass).addClass('past');
+				}
+			}
+		}
+	}
+
+	if (!isequalFlag) {
+		if (ampm === 'am') {
+			for (var m = 0; m < hourArray.length; m++) {
+				var futureClass = '.todo.' + hourArray[m];
+				$(futureClass).addClass('future');
+			}
+		} else {
+			for (var p = 0; p < hourArray.length; p++) {
+				var pastClass = '.todo.' + hourArray[p];
+				$(pastClass).addClass('past');
+			}
+		}
+	}
+
+	console.log(currentHour);
+	console.log(ampm);
 }
 
 renderTime();
 renderTodos();
+renderColors();
